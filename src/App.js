09277c6ab03ef80b0b7063944bec797be08c958c
nabilhayet <nabil4457@yahoo.com>
2022-12-06 13:28:24 -0500

    setup project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app_body">
+        < Sidebar />
+      </div>
+    </div >
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-yWQAT_0yKi10EiI424tBKRLyTIMJoQI",
+    authDomain: "whatsapp-clone-8071a.firebaseapp.com",
+    projectId: "whatsapp-clone-8071a",
+    storageBucket: "whatsapp-clone-8071a.appspot.com",
+    messagingSenderId: "754789515454",
+    appId: "1:754789515454:web:e503087197dc0049d95eef",
+    measurementId: "G-81Q2D95GXV"
+};
