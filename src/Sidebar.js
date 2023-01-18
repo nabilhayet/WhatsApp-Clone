f9c3c115860cde60c6bc1ff837d18e371c92c319
nabilhayet <nabil4457@yahoo.com>
2023-01-18 11:41:50 -0500

    added sidebarchat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import ChatIcon from "@material-ui/icons/Chat"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
 import "./Sidebar.css"
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
                 <Avatar />
+                <div classname="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="sidebar_search">
-
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start chat" type="text" />
+                </div>
             </div>
-
             <div className="sidebar_chats">
-
+                <SidebarChat />
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
+                <h1>Sidebar Chat</h1>
             </div>
         </div>
     )
